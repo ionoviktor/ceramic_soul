54fd0036755bb485b77c484a24950b66eac535bc
Viktor <ionoviktor16@gmail.com>
2025-04-19 00:32:34 +0500

    added slider section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,42 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import 'purecss/build/grids-min.css';
 import 'purecss/build/grids-responsive-min.css';
 
 import "/src/sass/style.scss";
 
+
+
+
+try {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".icon-right-open",
+      prevEl: ".icon-left-open",
+    },
+    breakpoints: {
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1920: {
+        slidesPerView: 3,
+        spaceBetween: 35,
+      },
+    },
+    modules: [Navigation, Pagination],
+  });
+
+} catch (e) {}
